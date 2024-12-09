0aa56b95b544714b0851f93e5a72cfeabc1b3411
Connor Warme <connor.warme@gmail.com>
2024-12-09 11:12:04 -0700

    feat: add assets, setup nativewind, create constants

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+// export default function RootLayout() {
+//   return <Stack />;
+// }
+
+import { Slot } from "expo-router";
+import "../assets/styles/global.css";
+
+export default Slot;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function Index() {
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello World</Text>
     </View>
   );
-}
+};
+
+export default App;
